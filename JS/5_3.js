40d9e6d57257be87cbdcf9fdb077205b525b7895
VHromyk <hromykk@gmail.com>
2020-10-26 18:03:06 +0200

    add tasks for 5-JS module

---
+++ JS/5_3.js
@@ -0,0 +1,62 @@
+// Задача 5-3
+/*использование методов класса
+Напиши класс Storage, который будет создавать объекты для управления складом товаров. 
+При вызове будет получать один аргумент - начальный массив товаров, и записывать его в свойство items.
+
+Добавь методы класса:
+
+getItems() - возвращает массив текущих товаров
+addItem(item) - получает новый товар и добавляет его к текущим
+removeItem(item) - получает товар и, если он есть, удаляет его из текущих*/
+
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(product) {
+    this.items.push(product);
+  }
+  removeItem(noProduct) {
+    this.items = this.items.filter(item => item !== noProduct) 
+    console.log(this.items);
+    return this.items;
+  }
+}
+
+const goods = [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+];
+const storage = new Storage(goods);
+
+ console.log(storage.getItems());
+[
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+] 
+
+ storage.addItem('Дроид');
+console.log(storage.getItems());
+ [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] 
+
+storage.removeItem('Пролонгер');
+console.log(storage.getItems());
+[
+  'Нанитоиды',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] 
